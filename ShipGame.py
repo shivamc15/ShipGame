9db5242e6cc300fe5559b55b23d35d1d127c599e
chowmann <chowmann@users.noreply.github.com>
2020-11-28 12:04:09 -0800

    Refactored a little bit, created timer method for general timer processing

--- ShipGame.py
+++ ShipGame.py
@@ -2,8 +2,6 @@
 import turtle
 # This following line may have been redundant
 from turtle import Turtle
-# Now let's start the project
-from typing import Tuple
 
 wn = turtle.Screen()
 wn.bgcolor("black")
@@ -30,16 +28,16 @@ bullet1 = turtle.Turtle()
 bullet1.hideturtle()
 
 bullet1.penup()
-bullet1.color("blue")
+bullet1.color("orange")
 bullet1.shape("square")
 
 #Create Bomber
-bomber=turtle.Turtle()
+bomber = turtle.Turtle()
 bomber.speed(0)
 bomber.penup()
 bomber.color("green")
 bomber.shape("circle")
-bomber.setpos(300,300)
+bomber.setpos(300, 300)
 
 
 def turn_right():
@@ -48,7 +46,6 @@ def turn_right():
     direction = direction - 5
     if direction < 0:
         x = 0 - direction
-
         direction = 270 + x
 
 
@@ -60,22 +57,29 @@ def turn_left():
         x = 0 - direction
 
 
-def touchbomber(bulletx,bullety):
+def bullet_reset():
     global bulletIsFired
-    if bomber.distance(bulletx,bullety)<15:
+    bulletIsFired = 0
+    bullet1.hideturtle()
+    bullet1.penup()
+    bullet1.setpos(0, 0)
+
+
+def touchbomber(bulletx,bullety):
+    if bomber.distance(bulletx, bullety) < 15:
         bomber.speed(0)
-        bomber.setpos(1000,900)
-        bulletIsFired = 0
-        bullet1.hideturtle()
-        bullet1.penup()
-        bullet1.setpos(0, 0)
+        bomber.setpos(1000, 900)
+        bullet_reset()
+
+def timer():
+    bullet_timer()
+    wn.ontimer(timer, bulletDelay)
 
 
 def bullet_timer():
     global bulletIsFired
-    global currentbulletpos
 #    print("bullet timer called")
-    if bulletIsFired==1:
+    if bulletIsFired == 1:
 
         #print("bullet is fired")
         bullet1.forward(10)
@@ -83,17 +87,17 @@ def bullet_timer():
         touchbomber(currentbulletpos[0],currentbulletpos[1])
 
         if (isoutofbounds(currentbulletpos)):
-            bulletIsFired = 0
-            bullet1.hideturtle()
-            bullet1.penup()
-            bullet1.setpos(0, 0)
-    wn.ontimer(bullet_timer, bulletDelay)
+            bullet_reset()
 
 
 def isoutofbounds(currentbulletpos):
     # print(currentbulletpos[0])
     # print(currentbulletpos[1])
-    return(currentbulletpos[0] > 500 or currentbulletpos[0] < -500 or currentbulletpos[1] > 400 or currentbulletpos[1] < -400)
+    localx = currentbulletpos[0]
+    y = currentbulletpos[1]
+
+    # TODO: Try to use screensize method to do this test
+    return localx > 500 or localx < -500 or y > 400 or y < -400
 
 
 def shoot_bullet():
@@ -124,6 +128,6 @@ turtle.onkey(turn_left, "Left")
 turtle.onkey(shoot_bullet, "space")
 turtle.onkey(turtleend, "Escape")
 
-bullet_timer()
+timer()
 
 turtle.mainloop()
