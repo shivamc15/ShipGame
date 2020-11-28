fa0e90f9b696a5fce29304b50ad57222875b1089
shivamc15 <75148972+shivamc15@users.noreply.github.com>
2020-11-27 19:09:50 -0800

    Able to fire multiple bullets one at a time

--- ShipGame.py
+++ ShipGame.py
@@ -3,9 +3,11 @@ import turtle
 # This following line may have been redundant
 from turtle import Turtle
 # Now let's start the project
+from typing import Tuple
 
 wn = turtle.Screen()
 wn.bgcolor("black")
+wn.setup(width=1000, height=800)
 x = 0
 direction = 0
 
@@ -17,8 +19,10 @@ ship.shape("arrow")
 
 
 # Create Bullet State
+bulletx=0
+bullety=0
 bulletIsFired = 0
-bulletDelay = 100
+bulletDelay = 50
 
 
 
@@ -50,29 +54,49 @@ def turn_left():
     direction = direction + 5
     if direction < 0:
         x = 0 - direction
+#
 
 
 def bullet_timer():
+    global bulletIsFired
     print("bullet timer called")
-    if (bulletIsFired):
+    if bulletIsFired==1:
         print("bullet is fired")
         bullet1.forward(10)
         currentbulletpos = bullet1.pos()
-        print(currentbulletpos[0])
-        print(currentbulletpos[1])
+        if (isoutofbounds(currentbulletpos)):
+            bulletIsFired = 0
+            bullet1.hideturtle()
+            bullet1.penup()
+            bullet1.setpos(0, 0)
     wn.ontimer(bullet_timer, bulletDelay)
+
+
+def isoutofbounds(currentbulletpos):
+    # print(currentbulletpos[0])
+    # print(currentbulletpos[1])
+    return(currentbulletpos[0] > 500 or currentbulletpos[0] < -500 or currentbulletpos[1] > 400 or currentbulletpos[1] < -400)
+
 
 def shoot_bullet():
     global direction
     global bulletIsFired
+    global ship
 
     if bulletIsFired == 0:
         bulletIsFired = 1
-        bullet1.setheading(direction)
+#        bullet1.setheading(direction)
+        print(ship.heading())
+        bullet1.setheading(ship.heading())
         bullet1.showturtle()
 
-
     direction = 270 + x
+
+
+def turtleend():
+    global wn
+
+    wn.bye()
 
 
 # Setup Turtle
@@ -80,6 +104,7 @@ turtle.listen()
 turtle.onkey(turn_right, "Right")
 turtle.onkey(turn_left, "Left")
 turtle.onkey(shoot_bullet, "space")
+turtle.onkey(turtleend, "Escape")
 
 bullet_timer()
 
