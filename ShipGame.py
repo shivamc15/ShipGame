502ee9be8ebba332fb29750e14bdec3bc8e127c5
chowmann <chowmann@users.noreply.github.com>
2020-11-28 21:59:33 -0800

    Updated file to use ShipGameBomber - fixed restarting the bomber

--- ShipGame.py
+++ ShipGame.py
@@ -2,6 +2,7 @@
 import turtle
 # This following line may have been redundant
 from turtle import Turtle
+import ShipGameBomber
 
 wn = turtle.Screen()
 wn.bgcolor("black")
@@ -15,151 +16,10 @@ linebuilder.pencolor("white")
 linebuilder.penup()
 linebuilder.setpos(-300, -300)
 linebuilder.pendown()
-linebuilder.setpos(-300,300)
-linebuilder.setpos(300,300)
-linebuilder.setpos(300,-300)
-linebuilder.setpos(-300,-300)
-x = 0
-direction = 0
-
-# Create Ship
-ship = turtle.Turtle()
-ship.setheading(0)
-ship.color("red")
-ship.shape("arrow")
-
-# Create Bullet State
-
-bulletIsFired = 0
-bulletDelay = 3
-
-# Create Bullet
-bullet1 = turtle.Turtle()
-bullet1.hideturtle()
-
-bullet1.penup()
-bullet1.color("orange")
-bullet1.shape("square")
-
-# Create Bomber
-bomber = turtle.Turtle()
-bomber.speed(0)
-bomber.penup()
-bomber.color("green")
-bomber.shape("circle")
-bomber.setpos(300, 300)
-
-
-def turn_right():
-    global direction
-    ship.right(5)
-    direction = direction - 5
-    if direction < 0:
-        x = 0 - direction
-        direction = 270 + x
-
-
-def turn_left():
-    global direction
-    ship.left(5)
-    direction = direction + 5
-    if direction < 0:
-        x = 0 - direction
-
-
-def bullet_reset():
-    global bulletIsFired
-    bulletIsFired = 0
-    bullet1.hideturtle()
-    bullet1.penup()
-    bullet1.setpos(0, 0)
-
-
-def touchbomber(bulletx, bullety):
-    if bomber.distance(bulletx, bullety) < 15:
-        bomber.speed(0)
-        bomber.setpos(1000, 900)
-        bullet_reset()
-
-
-def timer():
-    bullet_timer()
-    bomber_timer()
-    wn.ontimer(timer, bulletDelay)
-
-
-def bomber_timer():
-    bomber.speed(2)
-    angletoship = bomber.towards(0, 0)
-    bomber.setheading(angletoship)
-    bomber.forward(10)
-    if bomber.distance(0, 0) < 15:
-        turtleend()
-
-
-def bullet_timer():
-    global bulletIsFired
-    #    print("bullet timer called")
-    if bulletIsFired == 1:
-
-        # print("bullet is fired")
-        bullet1.forward(10)
-        currentbulletpos = bullet1.pos()
-        touchbomber(currentbulletpos[0], currentbulletpos[1])
-
-        if (isoutofbounds(currentbulletpos)):
-            bullet_reset()
-
-
-def isoutofbounds(currentbulletpos):
-    # print(currentbulletpos[0])
-    # print(currentbulletpos[1])
-    localx = currentbulletpos[0]
-    y = currentbulletpos[1]
-
-    # TODO: Try to use screensize method to do this test
-    return localx > 500 or localx < -500 or y > 400 or y < -400
-
-
-def shoot_bullet():
-    global direction
-    global bulletIsFired
-    global ship
-
-    if bulletIsFired == 0:
-        bulletIsFired = 1
-        #        bullet1.setheading(direction)
-        print(ship.heading())
-        bullet1.setheading(ship.heading())
-        bullet1.showturtle()
-
-    direction = 270 + x
-
-
-def turtleend():
-    global wn
-
-    wn.bye()
-
-
-# Setup Turtle
-turtle.listen()
-turtle.onkey(turn_right, "Right")
-turtle.onkey(turn_left, "Left")
-turtle.onkey(shoot_bullet, "space")
-turtle.onkey(turtleend, "Escape")
-
-timer()
-
-turtle.mainloop()
-
-linebuilder.penup()
+linebuilder.setpos(-300, 300)
+linebuilder.setpos(300, 300)
+linebuilder.setpos(300, -300)
 linebuilder.setpos(-300, -300)
-linebuilder.pendown()
-linebuilder.setpos(-300,300)
-linebuilder.setpos(300,300)
-linebuilder.setpos(300,-300)
-linebuilder.setpos(-300,-300)
 x = 0
 direction = 0
 
@@ -183,13 +43,9 @@ bullet1.color("orange")
 bullet1.shape("square")
 
 # Create Bomber
-bomber = turtle.Turtle()
-bomber.speed(0)
-bomber.penup()
-bomber.color("green")
-bomber.shape("circle")
-bomber.setpos(300, 300)
-
+bomber1 = ShipGameBomber.Bomber(300, 300)
+bomber1.makeactive()
+bomberlist = [bomber1]
 
 def turn_right():
     global direction
@@ -216,12 +72,11 @@ def bullet_reset():
     bullet1.setpos(0, 0)
 
 
-def touchbomber(bulletx, bullety):
-    if bomber.distance(bulletx, bullety) < 15:
-        bomber.speed(0)
-        bomber.setpos(1000, 900)
+def touchbomber(bomber, bullet):
+    if bomber.istouchingBullet(bullet):
+        bomber.makeinactive()
         bullet_reset()
-
+        wn.ontimer(bomber.makeactive, 1000)
 
 def timer():
     bullet_timer()
@@ -230,36 +85,32 @@ def timer():
 
 
 def bomber_timer():
-    bomber.speed(2)
-    angletoship = bomber.towards(0, 0)
-    bomber.setheading(angletoship)
-    bomber.forward(10)
-    if bomber.distance(0, 0) < 15:
-        turtleend()
+    global bomberlist
+    for bomber in bomberlist:
+        bomber.moveforward()
+        if bomber.ishittingship():
+            turtleend()
 
 
 def bullet_timer():
     global bulletIsFired
-    #    print("bullet timer called")
+    global bomberlist
+
     if bulletIsFired == 1:
-
-        # print("bullet is fired")
         bullet1.forward(10)
-        currentbulletpos = bullet1.pos()
-        touchbomber(currentbulletpos[0], currentbulletpos[1])
+        for bomber in bomberlist:
+            touchbomber(bomber, bullet1)
 
-        if (isoutofbounds(currentbulletpos)):
+        if (isoutofbounds(bullet1.pos())):
             bullet_reset()
 
 
 def isoutofbounds(currentbulletpos):
-    # print(currentbulletpos[0])
-    # print(currentbulletpos[1])
     localx = currentbulletpos[0]
     y = currentbulletpos[1]
 
     # TODO: Try to use screensize method to do this test
-    return localx > 500 or localx < -500 or y > 400 or y < -400
+    return localx > 300 or localx < -300 or y > 300 or y < -300
 
 
 def shoot_bullet():
@@ -279,7 +130,7 @@ def shoot_bullet():
 
 def turtleend():
     global wn
-
+    print("Turtle end called")
     wn.bye()
 
 
@@ -293,3 +144,4 @@ turtle.onkey(turtleend, "Escape")
 timer()
 
 turtle.mainloop()
+
